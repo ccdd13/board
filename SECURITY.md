3bb51f771c2aec7fe2498eafcddb14e1901bd1e9
ccdd13 <78702084+ccdd13@users.noreply.github.com>
2021-02-19 17:03:31 +0100

    Revert "update dependencies"

---
+++ SECURITY.md
@@ -0,0 +1,8 @@
+# Security Policy
+
+## Reporting a Vulnerability
+
+:turtle: i may be slow to respond
+
+- https://github.com/ccdd13/board
+- report to https://github.com/baianat/board
--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import test, { ExecutionContext } from "ava";
-
-test.before(async (t: ExecutionContext) => {
-  t.log(new Date());
-});
-
-test("test", async (t: ExecutionContext) => {
-  t.is("A", "A");
-});
